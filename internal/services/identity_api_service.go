bb2c799d10096978cfb0183423105d12a51bd3df
Allyson-English <allyson.english@gmail.com>
2024-07-01 17:11:27 -0400

    genqlient

--- internal/services/identity_api_service.go
+++ internal/services/identity_api_service.go
@@ -2,14 +2,11 @@ package services
 
 import (
 	"accounts-api/internal/config"
-	"bytes"
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 	"time"
 
+	"github.com/Khan/genqlient/graphql"
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -18,56 +15,29 @@ const (
 	jsonContentType      = "application/json"
 )
 
+var minimumConnections = 1
+
 type IdentityService interface {
 	AftermarketDevicesOwned(ctx context.Context, ethAddr common.Address) (bool, error)
 	VehiclesOwned(ctx context.Context, ethAddr common.Address) (bool, error)
 }
 
 type identityAPI struct {
-	url    string
-	client *http.Client
+	client graphql.Client
 }
 
 func NewIdentityService(settings *config.Settings) IdentityService {
+	graphqlClient := graphql.NewClient(settings.IdentityAPIURL, &http.Client{
+		Timeout: 5 * time.Second,
+	})
 	return &identityAPI{
-		url: settings.IdentityAPIURL,
-		client: &http.Client{
-			Timeout: 5 * time.Second,
-		}}
+		client: graphqlClient,
+	}
 }
 
 func (i *identityAPI) VehiclesOwned(ctx context.Context, ethAddr common.Address) (bool, error) {
-	payloadBytes, err := json.Marshal(GraphQLRequest{
-		Query: fmt.Sprintf(`{vehicles(first: 1,filterBy:{owner: "%s"}){nodes{tokenId}}}`, ethAddr.Hex()),
-	})
-	if err != nil {
-		return false, err
-	}
-
-	reader := bytes.NewReader(payloadBytes)
-	req, err := http.NewRequest(http.MethodPost, i.url, reader)
-	if err != nil {
-		return false, err
-	}
-
-	req.Header.Set(contentTypeHeaderKey, jsonContentType)
-	resp, err := i.client.Do(req.WithContext(ctx))
-	if err != nil {
-		return false, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return false, err
-	}
-
-	var id IdentityVehicleResponse
-	if err := json.Unmarshal(body, &id); err != nil {
-		return false, err
-	}
-
-	if len(id.Data.Vehicles.Nodes) > 0 {
+	resp, err := vehicles(ctx, i.client, &minimumConnections, &VehiclesFilter{Owner: &ethAddr})
+	if len(resp.Vehicles.GetNodes()) > 0 {
 		return true, nil
 	}
 
@@ -75,63 +45,10 @@ func (i *identityAPI) VehiclesOwned(ctx context.Context, ethAddr common.Address)
 }
 
 func (i *identityAPI) AftermarketDevicesOwned(ctx context.Context, ethAddr common.Address) (bool, error) {
-	payloadBytes, err := json.Marshal(GraphQLRequest{
-		Query: fmt.Sprintf(`{aftermarketDevices(first: 1,filterBy:{owner: "%s"}){nodes{tokenId}}}`, ethAddr.Hex()),
-	})
-	if err != nil {
-		return false, err
-	}
-
-	reader := bytes.NewReader(payloadBytes)
-	req, err := http.NewRequest(http.MethodPost, i.url, reader)
-	if err != nil {
-		return false, err
-	}
-
-	req.Header.Set(contentTypeHeaderKey, jsonContentType)
-	resp, err := i.client.Do(req.WithContext(ctx))
-	if err != nil {
-		return false, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return false, err
-	}
-
-	var id IdentityAftermarketDeviceResponse
-	if err := json.Unmarshal(body, &id); err != nil {
-		return false, err
-	}
-
-	if len(id.Data.AftermarketDevices.Nodes) > 0 {
+	resp, err := aftermarketDevices(ctx, i.client, &minimumConnections, &AftermarketDevicesFilter{Owner: &ethAddr})
+	if len(resp.AftermarketDevices.GetNodes()) > 0 {
 		return true, nil
 	}
 
 	return false, err
-}
-
-type GraphQLRequest struct {
-	Query string `json:"query"`
-}
-
-type IdentityVehicleResponse struct {
-	Data struct {
-		Vehicles struct {
-			Nodes []struct {
-				TokenID int `json:"tokenId"`
-			} `json:"nodes"`
-		} `json:"vehicles"`
-	} `json:"data"`
-}
-
-type IdentityAftermarketDeviceResponse struct {
-	Data struct {
-		AftermarketDevices struct {
-			Nodes []struct {
-				TokenID int `json:"tokenId"`
-			} `json:"nodes"`
-		} `json:"aftermarketDevices"`
-	} `json:"data"`
 }
